62a42f3bc60a41ce6d284dc7a6f6fb58b8a9a699
Nguyen Viet <nguyenviet02.dev@gmail.com>
2025-10-30 14:49:06 +0700

    feat: sign in + sign up form

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,11 @@
+import SigninForm from '@/components/ui/signIn';
+
+const SignInPage = () => {
+  return (
+    <div className="relative w-full flex items-center justify-center font-sans overflow-hidden">
+      <SigninForm />
+    </div>
+  );
+};
+
+export default SignInPage;
---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,11 @@
+import SignupForm from '@/components/ui/signUp';
+
+const SignUpPage = () => {
+  return (
+    <div className="relative w-full flex items-center justify-center font-sans overflow-hidden">
+      <SignupForm />
+    </div>
+  );
+};
+
+export default SignUpPage;
